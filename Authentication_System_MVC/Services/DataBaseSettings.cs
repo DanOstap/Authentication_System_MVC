45d18a37a0c123f3381f572b944e961a1bba0f52
Dan_Ostap <84470259+DanOstap@users.noreply.github.com>
2023-10-27 22:48:06 +0200

    Update DataBaseSettings.cs

--- Authentication_System_MVC/Services/DataBaseSettings.cs
+++ Authentication_System_MVC/Services/DataBaseSettings.cs
@@ -2,7 +2,7 @@
 {
     public class DataBaseSettings
     {
-        public string ConnectionUrl { get; set; } = "mongodb+srv://dan_ostap:Frost310@cluster0.1ecvuza.mongodb.net/?retryWrites=true&w=majority ";
+        public string ConnectionUrl { get; set; } = "";
         public string DataBaseMane { get; set; } = "LogIn_Service";
         public string Collection { get; set; } = "Users";
 
